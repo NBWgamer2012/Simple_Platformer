00392c9c7327b41d809f7265bf04e61909befc29
NBWgamer2012 <nwaterman2012@gmail.com>
2026-07-25 14:55:01 -0500

    this should fix audio for web
    added a new routine for load audio when not on PC.

--- CODE/audio_player.gd
+++ CODE/audio_player.gd
@@ -7,11 +7,18 @@ func _ready():
 		if str(get_tree().current_scene.name) == "Main":
 			music_loaded = true
 			if len(shared.cur_stage_start_positions[int(shared.level_number)]) > 3:
-				if FileAccess.file_exists("res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3])):
-					self.stream = AudioStreamOggVorbis.load_from_file(str(("res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3]))))
-					music_text_show(str(shared.cur_stage_start_positions[int(shared.level_number)][3]))
+				if OS.has_feature("pc"):
+					if FileAccess.file_exists("res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3])):
+						self.stream = AudioStreamOggVorbis.load_from_file(str(("res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3]))))
+						music_text_show(str(shared.cur_stage_start_positions[int(shared.level_number)][3]))
+					else:
+						shared.error.append(str("could not load audio track at", "res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3])))
 				else:
-					shared.error.append(str("could not load audio track at", "res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3])))
+					if ResourceLoader.exists(str(("res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3])))):
+						self.stream = load(str(("res://assets/Audio/tracks/" + str(shared.cur_stage_start_positions[int(shared.level_number)][3]))))		
+						music_text_show(str(shared.cur_stage_start_positions[int(shared.level_number)][3]))
+					else:
+						shared.error.append(str("could not load audio track " + str(shared.cur_stage_start_positions[int(shared.level_number)][3])))
 			else:
 				shared.error.append(str("could not get music from array, check if item 3 exists"))
 				
